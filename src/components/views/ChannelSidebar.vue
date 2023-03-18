dd05d1720496d79ea8d168fa8efe00956f9392a0
Raghav Kaushik <raghav.kaushik611@gmail.com>
2023-03-18 21:39:25 +0530

    sidebar

---
+++ src/components/views/ChannelSidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    <aside class="sidebar">
+        <nav>
+            <div class="channel">JD</div>
+            <div class="channel">MK</div>
+            <div class="channel">AK</div>
+        </nav>
+    </aside>
+</template>
+
+<style scoped>
+.sidebar {
+    width: 50px;
+    background-color: #2e283f;
+}
+
+nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    padding: 20px 0;
+}
+
+.channel {
+    background-color: crimson;
+    color: whitesmoke;
+    font-size: 14px;
+    border-radius: 50%;
+    font-weight: bold;
+    width: 32px;
+    height: 32px;
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+    transition: all 0.5s ease;
+}
+
+.channel:hover {
+    scale: 1.03;
+    box-shadow: 1px 1px 8px rgba(172, 255, 47, 0.5);
+}
+</style>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,13 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import SendMessage from '@/components/views/SendMessage.vue';
+import ChannelSidebar from '../components/views/ChannelSidebar.vue';
 </script>
 
 <template>
     <div class="app-layout-container">
-        <aside class="sidebar">
-            <nav></nav>
-        </aside>
+        <ChannelSidebar />
 
         <main>
             <RouterView />
@@ -18,12 +17,17 @@ import SendMessage from '@/components/views/SendMessage.vue';
 </template>
 
 <style scoped>
+.app-layout-container {
+    display: flex;
+}
+
+
 main {
+    flex: 1;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     min-height: 100vh;
     padding: 20px 10px;
 }
-
 </style>
