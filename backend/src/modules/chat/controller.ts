d2883a755813690140f0b8ab62e0090ca96a7153
Raghav Kaushik <raghav.kaushik611@gmail.com>
2023-03-31 23:48:02 +0530

    bug fix

--- backend/src/modules/chat/controller.ts
+++ backend/src/modules/chat/controller.ts
@@ -17,10 +17,12 @@ export async function listMessagesHandler(
     if (!channel) {
       throw new HttpException(400, "No such channel");
     }
-console.log(12312)
+
     const response = await listMessages(channel);
 
-    reply.send(response);
+    // Had to return reply here due to an error in fastify/compress
+    // https://github.com/fastify/fastify-compress/issues/215#issuecomment-1210598312
+    return reply.send(response);
   } catch (error) {
     console.log(error)
     throw new HttpException(500, "Something went wrong");
